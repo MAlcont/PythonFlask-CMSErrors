4267b62bc6e35cc3d6ca3018596c3cdfd1686516
corvotorvo <83085402+MAlcont@users.noreply.github.com>
2022-07-04 09:24:44 +0200

    Completed module

--- cms/handlers.py
+++ cms/handlers.py
@@ -1,6 +1,19 @@
 ## Imports
 from flask import request, render_template
+from logging import getLogger
+from logging.handlers import RotatingFileHandler
+from time import strftime
 
 from cms import app
 from cms.admin.models import Content, Type
 #!
+request_log= getLogger('werkzeug', disabled = True)
+
+def configure_logging(name, level):
+    log= getLogger(name)
+    log.setLevel(level)
+    handler= RotatingFileHandler('logs/{}.log'.format(name), maxBytes=1024*1024, backupCount=10)
+    log.handler()
+    return log
+
+timestamp= strftime()
